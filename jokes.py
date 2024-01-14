50eaa0565b594da4cae2be2fd4cf4bc9150ba034
Raja Kumar <56065083+rajak7549@users.noreply.github.com>
2024-01-14 23:40:50 +0530

    Add files via upload

---
+++ jokes.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from datetime import date
+from tkinter import filedialog
+from tkinter import messagebox
+#from PIL import Image, ImageTk
+import os
+from tkinter.ttk import Combobox
+#import openpyxl,xlrd
+#from openpyxl import Workbook
+import pathlib
+
+background="#06283D"
+framebg="#EDEDED"
+framefg="#06283D"
+
+root=Tk()
+root.title("Student Registration System ")
+root.geometry("1250x700+210+100")
+root.config(bg=background)
+
+root.mainloop()
